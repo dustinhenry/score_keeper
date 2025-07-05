c934bcbb527b8195679acd15aa4b6b9fc00f809f
dustinhenry <dustinjameshenry@gmail.com>
2025-07-05 04:28:33 -0500

    Amended comments and docstrings

--- scoreKeeper.py
+++ scoreKeeper.py
@@ -1,9 +1,13 @@
 #! usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# Author: Dustin Henry
+# Date: 2025-07-05
+
 """
-@file: mayI.py
+@file: scoreKeeper.py
 @brief: A simple script to keep in game score, highest, lowest and average scores as well as the number of wins and games played.
+@version: 1.0
 """
 
 import random
