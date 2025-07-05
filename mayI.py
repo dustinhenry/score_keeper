6d8bc6a45ff44e3ab1f2784cc7b9bf5e01f5eec8
Dustin Henry <dustinjameshenry@gmail.com>
2025-07-04 21:33:49 -0500

    First Commit

---
+++ mayI.py
@@ -0,0 +1,9 @@
+#! usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+@file: mayI.py
+@brief: A simple script to keep in game score, highest, lowest and average scores as well as the number of wins and games played.
+"""
+
+import random
